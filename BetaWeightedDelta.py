8d99aa6361d1bbacda576105f5bf0bc44ca7f1d3
Abishai521 <abishai.markgundy@gmail.com>
2020-12-01 01:26:17 +0530

    Update BetaWeightedDelta.py

--- BetaWeightedDelta.py
+++ BetaWeightedDelta.py
@@ -15,6 +15,10 @@ st.write("""
 Democratization of Retail Investing!
 """)
 
+st.write("""
+# FILL IN ALL FIELDS TO REMOVE THE ERRORS!
+""")
+
 startDate = datetime.today().strftime('%Y-%m-%d')
 endDate = datetime.today().strftime('%Y-%m-%d')
 
@@ -104,4 +108,4 @@ sizes = shareCount
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode = explode, labels = labels, autopct='%1.1f%%', shadow = True, startangle = 90)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-st.pyplot()
+st.pyplot(fig1)
