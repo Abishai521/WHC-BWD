5d9972d0ccca88344bffbde3e10ed2f137a134ce
Abishai521 <abishai.markgundy@gmail.com>
2020-12-01 01:21:22 +0530

    Update BetaWeightedDelta.py

--- BetaWeightedDelta.py
+++ BetaWeightedDelta.py
@@ -102,6 +102,6 @@ st.write("""
 labels = list
 sizes = shareCount
 fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode = [0], labels = labels, autopct='%1.1f%%', shadow = True, startangle = 90)
+ax1.pie(sizes, explode = explode, labels = labels, autopct='%1.1f%%', shadow = True, startangle = 90)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 st.pyplot()
